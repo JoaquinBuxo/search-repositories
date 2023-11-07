f0b9341a0d5848d687463e9cf15d93101d07cc8b
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-08 00:19:27 +0100

    feat: create repository types

---
+++ app/types/repository.ts
@@ -0,0 +1,16 @@
+export interface Owner {
+  login: string;
+  avatar_url: string;
+}
+
+export interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  owner: Owner;
+  created_at: string;
+  updated_at: string;
+  topics: string[];
+  language: string;
+  stargazers_count: number;
+}
