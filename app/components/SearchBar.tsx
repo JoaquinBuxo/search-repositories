63f8852e31795a27ab76618f8c0012f05d237b25
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-08 00:17:03 +0100

    feat: create search bar component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState, FormEvent } from 'react';
+
+type SearchBarProps = {
+  onSearch: (query: string) => void;
+};
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type='submit'>Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
