912c1412da0802b3f0e01a52d2287ad973c31e7a
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-08 15:21:28 +0100

    style: add MUI to search bar component

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { useState, FormEvent } from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 type SearchBarProps = {
   onSearch: (query: string) => void;
@@ -15,14 +19,21 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
+    <Paper
+      component='form'
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      onSubmit={handleSubmit}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder='Search Github Repositories'
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <button type='submit'>Search</button>
-    </form>
+      <IconButton type='submit' sx={{ p: '10px' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
