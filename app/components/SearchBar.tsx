7501391275af1e8ee55799b2d86ad9daa8571f37
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-08 23:29:42 +0100

    refactor: update styles in search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, FormEvent } from 'react';
-import { Paper, InputBase, IconButton } from '@mui/material';
+import { Container, Paper, InputBase, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 type SearchBarProps = {
@@ -17,21 +17,29 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <Paper
-      component='form'
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-      onSubmit={handleSubmit}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder='Search Github Repositories'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <IconButton type='submit' sx={{ p: '10px' }} aria-label='search'>
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <Container maxWidth='lg'>
+      <Paper
+        component='form'
+        elevation={0}
+        sx={{
+          m: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          border: '1px solid #D9DFE4',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder='Search Github Repositories'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <IconButton type='submit' sx={{ p: '4px' }} aria-label='search'>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Container>
   );
 };
 
