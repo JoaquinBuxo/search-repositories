66fd261ad18e02d115ea3c60a8bff8f41cd8d4b0
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-09 22:44:56 +0100

    test: create unit tests for header

--- app/components/__tests__/Header.test.tsx
+++ app/components/__tests__/Header.test.tsx
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test('renders search input placeholder', () => {
-  render(<Header />);
-  const inputElement = screen.getByPlaceholderText(/Search/i);
-  expect(inputElement).toBeInTheDocument();
+describe('Header component', () => {
+  test('renders correctly', () => {
+    render(<Header />);
+    const appBarElement = screen.getByRole('banner');
+    expect(appBarElement).toBeInTheDocument();
+  });
+
+  test('contains a SearchBar component', () => {
+    render(<Header />);
+    const inputElement = screen.getByPlaceholderText(/Search/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test('the AppBar has the correct style', () => {
+    render(<Header />);
+    const appBarElement = screen.getByRole('banner');
+    expect(appBarElement).toHaveStyle({
+      backgroundColor: '#F6F8FA',
+      border: '1px solid #D9DFE4',
+    });
+  });
 });
