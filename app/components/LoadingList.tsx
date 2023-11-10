b1343cb0d5c8fc8cc6460021a121cf2244c36dc6
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-11-10 01:50:09 +0100

    test: create unit test for repository list

--- app/components/LoadingList.tsx
+++ app/components/LoadingList.tsx
@@ -3,7 +3,7 @@ import SkeletonCard from './SkeletonCard';
 
 const LoadingList = () => {
   return (
-    <Grid container spacing={3}>
+    <Grid container spacing={3} data-testid='loading-list'>
       {Array.from(new Array(12)).map((_, index) => (
         <Grid item xs={12} sm={6} md={3} key={index}>
           <SkeletonCard />
---
+++ app/components/__tests__/RepositoryList.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react';
+import { useContextRepository } from '../../hooks/useContextRepository';
+
+import RepositoryList from '../RepositoryList';
+
+jest.mock('../../hooks/useContextRepository', () => ({
+  useContextRepository: jest.fn(),
+}));
+
+describe('RepositoryList', () => {
+  test('displays LoadingList during loading', () => {
+    // Simulate isLoading true
+    (useContextRepository as jest.Mock).mockReturnValueOnce({
+      isLoading: true,
+    });
+
+    render(<RepositoryList />);
+
+    // Ensure LoadingList is rendered
+    expect(screen.getByTestId('loading-list')).toBeInTheDocument();
+  });
+
+  test('displays error message in case of error', () => {
+    (useContextRepository as jest.Mock).mockReturnValueOnce({
+      isError: true,
+      error: { message: 'Error message' },
+    });
+
+    render(<RepositoryList />);
+    expect(screen.getByText('Error message')).toBeInTheDocument();
+  });
+
+  test('displays no results search message', () => {
+    (useContextRepository as jest.Mock).mockReturnValueOnce({
+      repositories: [],
+      query: 'searchQuery',
+    });
+
+    render(<RepositoryList />);
+    expect(
+      screen.getByText('No results found. Please try another search.')
+    ).toBeInTheDocument();
+  });
+
+  test('displays start message without search term', () => {
+    (useContextRepository as jest.Mock).mockReturnValueOnce({
+      repositories: [],
+    });
+
+    render(<RepositoryList />);
+    expect(
+      screen.getByText('Please enter a search term to start.')
+    ).toBeInTheDocument();
+  });
+});
